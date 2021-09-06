dbab6cfeb87b4aec7b9d134d0f5cb7cbd088c1fb
Georgia <georgia.mcnamara@students.mq.edu.au>
2021-09-06 11:39:53 +1000

    Initial commit

---
+++ tests/testthat/test-say.R
@@ -0,0 +1,15 @@
+test_that("input is in the correct format", {
+  what <- c("This is a character string")
+  say(what)
+  expect_type(what, "character")
+  expect_gt(length(what), 0)
+})
+
+# test the output is of correct format
+test_that("output is in the correct format", {
+  expect_length(say(), 0) #Returns something with a length of 0
+  expect_null(say()) #Returns null
+  expect_invisible(say()) #Returns invisibly
+  expect_message(say()) #Returns a message
+})
+
